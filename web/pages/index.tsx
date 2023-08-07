a6433185c8bf3b672c7891309a896febfec801e5
parazinshow <victorfretes32@gmail.com>
2023-08-07 09:57:27 -0400

    first commit

---
+++ web/pages/index.tsx
@@ -0,0 +1,20 @@
+interface HomeProps {
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <div>Count: {props.count}</div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count');
+  const data = await response.json();
+  console.log(data);
+  return {
+    props: {
+      count: data.count
+    }
+  }
+}
